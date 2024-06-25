4bcb9fea6aca159b6eb2c802e955bf3b5ececbe2
RWn-ChetanSharma <rwa5.chetan.sha@gmail.com>
2024-06-25 18:34:38 +0530

    Lecture Data

---
+++ C_language/02Pointer/08ChainPointerImg.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main(){
+	int a;    // &2006 
+	a = 10;   // value of a = 10;
+	
+	int *ptr1;   // &4080
+	ptr1 = &a;   // &2006
+	
+	int **ptr2;    // &8010
+	ptr2 = &ptr1;  // &4080
+	
+	int ***ptr3;     // &8500
+	ptr3 = &ptr2;  // &8010
+	
+	
+	printf("a => %u => %d\n", ptr1, *ptr1);
+	printf("ptr1 => %u => %u => %d\n", ptr2, *ptr2, **ptr2);
+	printf("ptr2 => %u => %u => %u => %d\n", ptr3, *ptr3, **ptr3, ***ptr3);
+	
+	
+}
